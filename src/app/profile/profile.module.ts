e35548ae0f1265a48e5a0d75a13c017cb6130092
Yan David <ydgcdeveloper@gmail.com>
2022-07-16 20:15:30 -0400

    add pro skeleton

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { ProfilePageRoutingModule } from './profile-routing.module';
 
 import { ProfilePage } from './profile.page';
+import { ProSkeletonComponent } from './pro-skeleton/pro-skeleton.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { ProfilePage } from './profile.page';
     IonicModule,
     ProfilePageRoutingModule
   ],
-  declarations: [ProfilePage]
+  declarations: [ProfilePage, ProSkeletonComponent]
 })
 export class ProfilePageModule {}
