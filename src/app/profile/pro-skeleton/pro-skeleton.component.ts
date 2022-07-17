9f480531663e0e4183c485b211e84ff7e67560c1
Yan David <ydgcdeveloper@gmail.com>
2022-07-16 20:17:08 -0400

    add pro skeleton component class

---
+++ src/app/profile/pro-skeleton/pro-skeleton.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pro-skeleton',
+  templateUrl: './pro-skeleton.component.html',
+  styleUrls: ['./pro-skeleton.component.scss'],
+})
+export class ProSkeletonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
